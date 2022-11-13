750dc18ed789346606669b2ddd112c0dd7be6426
Pablo M <jpmora123@yahoo.com>
2022-11-12 21:26:05 -0500

    LetConstArrowFx Exercises

---
+++ Let & Const Exercise.js
@@ -0,0 +1,20 @@
+// Let & Const Exercise:
+
+// 1. What is the difference between var and let?
+
+// Var is function scoped and Let is block scoped. Variable declared by Let
+// cannot be redeclared and must be declared before use. Variables declared with
+// Var can be hoisted.
+
+// 2. What is the difference between var and const? 
+
+// You cannot redeclare or reassing using the Const keyword. Const declared Variables
+// can be hoisted while Const creates block scoped.
+
+// 3. What is the difference between Let & Const? 
+
+// You cannot redeclare or reassign using the const keyword. You can reassign with Let. 
+
+// 4. What is hoisting?
+
+// Hoisting allows you to use functions and variables before they are declared.
---
+++ arrowFunctionExercise.js
@@ -0,0 +1,24 @@
+Arrow Functions Exercise:
+
+// function double(arr) {
+//   return arr.map(function(val) {
+//     return val * 2;
+//   });
+// }
+
+Arrow function shorthand:
+
+const double = array => array.map(value => value * 2)
+
+
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+
+  const squareAndFindEvens = numbers => numbers.map(value => value ** 2).filter(square % 2 === 0)
